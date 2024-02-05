1f630a8c4f88c5e037393a441c1ca7213b7ad694
rasatria01 <rafiqsatri@gmail.com>
2024-02-05 21:26:31 +0700

    add mysql database

--- main.go
+++ main.go
@@ -2,16 +2,31 @@ package main
 
 import (
 	"net/http"
+	"os"
 	"todomono/controllers"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/rs/zerolog"
 )
 
 func main() {
 	e := echo.New()
+	logger := zerolog.New(os.Stdout)
+	e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogURI:     true,
+		LogStatus:  true,
+		LogLatency: true,
+		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
+			logger.Info().
+				Str("URI", v.URI).
+				Int("Status", v.Status).
+				Int("Duration", int(v.Latency)).
+				Msg("request")
 
-	e.Use(middleware.Logger())
+			return nil
+		},
+	}))
 	e.Use(middleware.Recover())
 
 	e.GET("/", func(c echo.Context) error {
